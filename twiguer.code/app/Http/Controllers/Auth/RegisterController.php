98aa10ecc17b295e0214a26c786c406dfd4e0fec
Omar Orozco <ing.omar.orozco@gmail.com>
2017-02-05 16:52:28 -0400

    creacion del proyecto base

---
+++ twiguer.code/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\RegistersUsers;
+
+use Illuminate\Http\Request;
+use App\ActivationService;
+
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+	
+	protected $activationService;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(ActivationService $activationService)
+    {
+        $this->middleware('guest');
+		$this->activationService = $activationService;
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|max:255',
+            'phone' => 'required|email|max:255|unique:users',
+			'phone' => 'required|max:100|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return User
+     */
+    protected function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+			'phone' => $data['phone'],
+            'password' => bcrypt($data['password']),
+        ]);
+    }
+	
+	public function register(Request $request)
+	{
+		$validator = $this->validator($request->all());
+
+		if ($validator->fails()) {
+			$this->throwValidationException(
+				$request, $validator
+			);
+		}
+		
+		$user = $this->create($request->all());
+
+		$this->activationService->sendActivationMail($user);
+
+		return redirect('/login')->with('status', 'We sent you an activation code. Check your email.');
+	}
+	
+	public function activateUser($token)
+	{
+		if ($user = $this->activationService->activateUser($token)) {
+			auth()->login($user);
+			return redirect($this->redirectPath());
+		}
+		abort(404);
+	}
+}
